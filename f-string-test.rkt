0b090d5ceb9c8f42a794053dfa215ddaf29ced32
Aryeh Hillman <aryeh.hillman@gmail.com>
2023-09-22 21:25:38 -0700

    Support for multiple interpolations

--- f-string-test.rkt
+++ f-string-test.rkt
@@ -4,5 +4,6 @@
 
 (require megaparsack megaparsack/text)
 
-(check-equal? (f-string/parse "{123}") 123)
-(check-equal? (f-string/parse "{\"123\"}") "123")
+(check-equal? (f-string/parse "{123}") "123")
+(check-equal? (f-string/parse "{123}{123}") "123123")
+; (check-equal? (f-string/parse "hello {\"xyz\"}") "hello xyz")
--- f-string.rkt
+++ f-string.rkt
@@ -1,21 +1,30 @@
 #lang racket
 
-(provide f-string/p f-string/parse)
+(provide f-string/expr/p f-string/parse)
 
 (require megaparsack megaparsack/text)
 (require data/monad data/applicative)
 
-(define f-string/p
+(define f-string/expr/p
   (do
     (char/p #\{)
-    [expression <- (many+/p (char-not-in/p "{}"))]
+    [expr <- (many+/p (char-not-in/p "{}"))]
     (char/p #\})
     (pure
-      (list->string expression))))
+      (read (open-input-string (list->string expr))))))
+
+(define f-string/p
+  (many/p
+    (do [datum <- f-string/expr/p]
+      (pure datum))))
+
+(define (f-string/read text)
+  (read
+    (open-input-string
+      text)))
 
 (define (f-string/parse text)
-    (read
-      (open-input-string
-        (parse-result!
-          (parse-string f-string/p text)))))
-
+  (string-join
+    (map (λ (v) (format "~v" v))
+      (parse-result!
+        (parse-string f-string/p text))) ""))
